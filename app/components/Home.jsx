1e2781abe46a3dd07cb00672ffc005154e76fe9c
David Thomas Grübl <me@davidgruebl.com>
2015-05-18 19:01:00 +0200

    feat(home): add login fields and log location

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,29 +1,34 @@
-import { Reapp, React, NestedViewList, View, Button } from 'reapp-kit';
+import { Reapp, React, NestedViewList, View, Button, Input } from 'reapp-kit'
 
 class Home extends React.Component {
-  render() {
+
+  getLocation () {
+    window.navigator.geolocation.getCurrentPosition(function (location) {
+      console.log(location)
+    })
+  }
+
+  render () {
+    this.getLocation()
     return (
       <NestedViewList {...this.props.viewListProps}>
-        <View title="track-dtg-admin-v2">
-          <p>Hello, World!</p>
-
+        <View title='track-dtg-admin-v2'>
+          <p>enter your credentials to get started</p>
+          <Input Input name='name' placeholder='username' />
+          <Input name='password' type='password' placeholder='password' />
+          <br/>
+          <Button>
+            login
+          </Button>
+          <br/>
           <Button onTap={() => this.router().transitionTo('sub')}>
-            Go to sub view
+            Legal
           </Button>
         </View>
-
         {this.props.child()}
       </NestedViewList>
-    );
+    )
   }
 }
 
-export default Reapp(Home);
-
-/*
- This is your root route. When you wrap it with Reapp()
- it passes your class two properties:
-
-  - viewListProps: Passes the scrollToStep to your ViewList so it animates
-  - child: The child route
-*/
+export default Reapp(Home)
